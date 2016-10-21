7aa77d5bbd509b4b17b3b404c5db63ae6fb4455e
Aralya Phinith <aphinith84@gmail.com>
2016-10-20 20:59:27 -0700

    boxes change to random color upon clicking

--- boxes/public/js/app.js
+++ boxes/public/js/app.js
@@ -1,1 +1,17 @@
-console.log('Connected!');
+//Change colors of boxes to random color upon clicking
+
+var changeColor = document.querySelectorAll(".second_row div");
+
+function randomColor(e) {
+  var box = e.target;
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  box.style.backgroundColor = color;
+}
+
+changeColor.forEach(function(val) {
+  val.addEventListener("click", randomColor);
+})
